5c1ed23983f34edc3beb31f237444adc8c254a8f
Axel Klein <axel.klein.1@ens.etsmtl.ca>
2024-10-01 13:41:23 -0400

    Finished Cristian without test

--- log736-labo1/Cristian_API/classes/Client.java
+++ log736-labo1/Cristian_API/classes/Client.java
@@ -2,10 +2,9 @@ package classes;
 
 import interfaces.IClient;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client implements IClient {
 
@@ -13,20 +12,42 @@ public class Client implements IClient {
 
     private long time0;
     private  long time1;
-    private long timeS;
+    private Long timeS;
 
     private long accuracy;
+    private long meanArccuracy=0;
+    private Socket socket;
+    private DataInputStream dis;
+    private DataOutputStream dos;
 
+    public Client(int serverPort) throws IOException {
+        this.socket = new Socket("127.0.0.1",serverPort);
+        System.out.println("Connected");
+    }
 
     public void requestTime(int serverPort, long currentTime, int numberOfTries) throws IOException {
-        Socket socket = new Socket("127.0.0.1",serverPort);
-        DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+        Scanner scanner = new Scanner(System.in);
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        dos = new DataOutputStream(socket.getOutputStream());
+        String messageSent = scanner.nextLine();
+        out.write(messageSent);
+        out.newLine();
+        out.flush();
+        if(messageSent.equals("close")){
+            endConnection();
+            return;
+        }
+        System.out.println("Message sent");
+        dis = new DataInputStream(socket.getInputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        System.out.println("Message received");
         time0 = System.nanoTime();
-        timeS = dis.readLong();
+        timeS = Long.parseLong(in.readLine());
         time1=System.nanoTime();
-        System.out.println(timeS+ " aaaa");
-        dis.close();
-        socket.close();
+        System.out.println(timeS.toString());
+
         setTime(timeS);
     }
 
@@ -36,6 +57,7 @@ public class Client implements IClient {
 
     public void setTime(long newTime) {
         this.accuracy = getAccuracy();
+        System.out.println("Old time -> "+this.time);
         this.time = newTime+this.accuracy;
         System.out.println(this.accuracy+"<- acc|time-> " + this.time);
     }
@@ -44,17 +66,36 @@ public class Client implements IClient {
         return (time1-time0)/2;
     }
 
-    public static void main(String[] args) {
-        Client client = new Client();
-        int nbOfTries=0;
+    public void setMeanAccuracy(long accuracy,int nbOfTries) {
+        this.meanArccuracy =((this.meanArccuracy*(nbOfTries-1)) +accuracy)/nbOfTries;
+    }
+    public long getMeanAccuracy() {return this.meanArccuracy;}
+    public void endConnection() {
         try {
-            nbOfTries++;
-
-            client.requestTime(25000, client.getTime(), nbOfTries);
-
+            this.socket.close();
+            this.dos.close();
+            this.dis.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) {
+        int nbOfTries=0;
+        try {
+            Client client = new Client(25000);
+            for(int i =0;i<nbOfTries+1;i++) {
+                nbOfTries++;
+
+                client.requestTime(25000, client.getTime(), nbOfTries);
+                client.setMeanAccuracy(client.getAccuracy(), nbOfTries);
+                System.out.println("Mean Accuracy -> "+client.getMeanAccuracy());
+            }
+            client.endConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
