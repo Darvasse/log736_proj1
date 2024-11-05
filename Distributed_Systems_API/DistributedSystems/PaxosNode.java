9f84f9175a6c69e6af458ab995eb6e2dd8b5ebe2
Patrick Quevillon <patrick.quevillon.work@gmail.com>
2024-11-05 12:51:44 -0500

    Copilot a la rescousse!

--- Distributed_Systems_API/DistributedSystems/PaxosNode.java
+++ Distributed_Systems_API/DistributedSystems/PaxosNode.java
@@ -19,6 +19,16 @@ public class PaxosNode extends Node {
     private long promisedThreshold = 0;
     private Integer value = null;
 
+    @Override
+    public boolean register() {
+        if(super.register()) {
+            ConnectedNodes.add(this);
+            return true;
+        }
+        return false;
+    }
+
+
     /**
      * Receives a message and processes it based on its subject.
      *
@@ -37,26 +47,64 @@ public class PaxosNode extends Node {
     }
 
     private void request(Message msg) {
-    
+        ProposalCount++;
+        Message prepare = new Message();
+        prepare.setSubject(PrepareCommand);
+        prepare.setHeader(String.valueOf(ProposalCount));
+        prepare.setContent(msg.getContent());
+        prepare.setTo(msg.getFrom());
+        API.broadcast(prepare);
     }
 
     private void prepare(Message msg) {
-    
+        int proposal = Integer.parseInt(msg.getHeader());
+        if(proposal > promisedThreshold) {
+            promisedThreshold = proposal;
+            Message promise = new Message();
+            promise.setSubject(PromiseCommand);
+            promise.setHeader(String.valueOf(proposal));
+            promise.setContent(value != null ? value.toString() : "");
+            promise.setTo(msg.getFrom());
+            API.send(promise, msg.getFrom());
+        }
     }
 
     private void accept(Message msg) {
-    
+        int proposal = Integer.parseInt(msg.getHeader());
+        if(proposal >= promisedThreshold) {
+            promisedThreshold = proposal;
+            value = Integer.parseInt(msg.getContent());
+            Message learn = new Message();
+            learn.setSubject(LearnCommand);
+            learn.setHeader(String.valueOf(proposal));
+            learn.setContent(value.toString());
+            API.broadcast(learn);
+        }
     }
 
     private void promise(Message msg) {
-    
+        int proposal = Integer.parseInt(msg.getHeader());
+        if(proposal == ProposalCount) {
+            int acceptedValue = msg.getContent().isEmpty() ? 0 : Integer.parseInt(msg.getContent());
+            if(acceptedValue > value) {
+                value = acceptedValue;
+            }
+            Message accept = new Message();
+            accept.setSubject(AcceptCommand);
+            accept.setHeader(String.valueOf(proposal));
+            accept.setContent(value.toString());
+            API.broadcast(accept);
+        }
     }
 
     private void learn(Message msg) {
-    
+        int proposal = Integer.parseInt(msg.getHeader());
+        if(proposal == ProposalCount) {
+            value = Integer.parseInt(msg.getContent());
+        }
     }   
 
     private void response(Message msg) {
-    
+        System.out.println("Node " + getUuid() + " received response: " + msg.toString());
     }
 }
